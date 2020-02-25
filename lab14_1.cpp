5725ca853095cb1fd99e59f8aefada6145e36b5c
lab302-15 <lab302-15@labeng.local>
2020-02-25 15:44:01 +0700

    lab14

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,9 +1,59 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 
 int main(){
-	//Write your code here.
+	int a = 5;
+	char b = 'A';
+	char &c = b;
+	int *x = &a;
+	char *y = &b;
+	int **z = &x;
+	cout << "a = " << a << endl; 
+	cout << "b = " << b << endl;
+	cout << "c = " << c << endl;
+	cout << "x = " << x << endl;
+	cout << "y = " << (int*)y << endl;
+	cout << "z = " << z << endl;
+	cout << "------------------------------\n";
+	cout << "&a = " << &a << endl; 
+	cout << "&b = " << (int*)&b << endl;
+	cout << "&c = " << (int*)&c << endl;
+	cout << "&x = " << &x << endl;
+	cout << "&y = " << &y << endl;
+	cout << "&z = " << &z << endl;
+	c = 'F';
+	cout << "------------------------------\n";
+	cout << "a = " << a << endl; 
+	cout << "b = " << b << endl;
+	cout << "c = " << c << endl;
+	cout << "x = " << x << endl;
+	cout << "y = " << (int*)y << endl;
+	cout << "z = " << z << endl;
+	*y = 'W';
+	cout << "------------------------------\n";
+	cout << "a = " << a << endl; 
+	cout << "b = " << b << endl;
+	cout << "c = " << c << endl;
+	cout << "x = " << x << endl;
+	cout << "y = " << (int*)y << endl;
+	cout << "z = " << z << endl;
+	*x = 6;
+	cout << "------------------------------\n";
+	cout << "a = " << a << endl; 
+	cout << "b = " << b << endl;
+	cout << "c = " << c << endl;
+	cout << "x = " << x << endl;
+	cout << "y = " << (int*)y << endl;
+	cout << "z = " << z << endl;
+	**z = 7;
+	cout << "------------------------------\n";
+	cout << "a = " << a << endl; 
+	cout << "b = " << b << endl;
+	cout << "c = " << c << endl;
+	cout << "x = " << x << endl;
+	cout << "y = " << (int*)y << endl;
+	cout << "z = " << z << endl;
 	
 	
 	
